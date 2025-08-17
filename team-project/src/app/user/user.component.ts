1a53a9f240ea53d66676c4e40d19ea182fd8e6b2
Anthony Villegas <villegasanthony87@yahoo.com>
2025-08-17 13:58:22 -0500

    feat: user component set-up

---
+++ team-project/src/app/user/user.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: "app-user",
+  standalone: true,
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
+})
+
+
+export class UserComponent {}
