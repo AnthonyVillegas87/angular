aae8db0fc6f459248ffa7d753861eae38ebb49bb
Anthony Villegas <villegasanthony87@yahoo.com>
2025-08-17 13:40:45 -0500

    feat: custom header component creaated

--- team-project/src/app/header.component.ts
+++ team-project/src/app/header.component.ts
@@ -1,9 +1,10 @@
 import {Component} from "@angular/core";
 
 @Component({
-  selector: "app-header",
+  selector: 'app-header',
   standalone: true,
-  templateUrl: "./header.component.html",
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 
 export class HeaderComponent {}
