3b471d149322ecbea14752818dd9792934ff3653
Anthony Villegas <villegasanthony87@yahoo.com>
2025-08-17 23:27:12 -0500

    feat: single task component set-up

--- team-project/src/app/tasks/tasks.component.ts
+++ team-project/src/app/tasks/tasks.component.ts
@@ -1,12 +1,45 @@
 import { Component, Input } from '@angular/core';
+import {TaskComponent} from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [
+    TaskComponent
+  ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required:true }) userId!: string;
+  @Input({ required:true }) name?: string;
+
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: '.NET 8 Migration',
+      summary: 'Update FMIS app with appropriate package builds',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'MAP 2027 updates',
+      summary: 'Implement new requirements for next FY',
+      dueDate: '2025-09-30'
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'UAP Buildout',
+      summary: 'UAP mvp for Energy Project',
+      dueDate: '2025-11-18'
+    }
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
--- team-project/src/app/user/user.component.ts
+++ team-project/src/app/user/user.component.ts
@@ -1,16 +1,7 @@
 import {Component, Input, Output, EventEmitter, output} from "@angular/core";
+import {type User } from './user.model'
 
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// };
 
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
 
 @Component({
   selector: "app-user",
